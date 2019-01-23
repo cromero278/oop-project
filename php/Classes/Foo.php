8baf822665e6f9985df02596dcf5c2ad95e0e961
Cassandra Romero <romero.cassandra1@gmail.com>
2019-01-23 11:49:23 -0700

    added directories and Foo code

---
+++ php/Classes/Foo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace comero278\ObjectOrientedProject;
+
+/**
+ * Author profile
+ *
+ * This class shows data collected and stored for an author profile, including email, username, avatar, etc.
+ *
+ * @author Cassandra Romero cromero278@cnm.edu
+ **/
+
+class Author {
+	/**
+	 * Id for this author, this is the primary key.
+	 **/
+	private $authorID;
+/**
+ * URL for the author's avatar.
+ */
+	private $authorAvatarURL;
+
+
+
+
+}
